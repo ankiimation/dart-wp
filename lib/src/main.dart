0ab39ca28c5dcbb70a7c1cf76a347826606cae72
anKii <41232001+ankiimation@users.noreply.github.com>
2021-06-30 13:19:05 +0700

    update

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -5,15 +5,25 @@ import 'package:wordpress_api/src/models.dart';
 import 'package:wordpress_api/src/utils.dart';
 
 part 'endpoints/application_passwords.dart';
+
 part 'endpoints/categories.dart';
+
 part 'endpoints/comments.dart';
+
 part 'endpoints/media.dart';
+
 part 'endpoints/pages.dart';
+
 part 'endpoints/posts.dart';
+
 part 'endpoints/users.dart';
+
 part 'endpoints/taxonomies.dart';
+
 part 'endpoints/tags.dart';
+
 part 'endpoints/job_listings.dart';
+
 part 'endpoints/search.dart';
 
 class WordPressAPI {
@@ -28,8 +38,7 @@ class WordPressAPI {
   /// WooCommerce API credentials
   final WooCredentials? wooCredentials;
 
-  WordPressAPI(
-    this.site, {
+  WordPressAPI(this.site, {
     this.wooCredentials,
     Dio? dio,
   }) : _dio = dio ??= Dio();
@@ -37,15 +46,16 @@ class WordPressAPI {
   // GET DATA FROM CUSTOM ENDPOINT //
   /// Retrieves data from a given endpoint and resturns a [WPResponse]
   Future<WPResponse> fetch(
-    /// Provide an API endpoint
-    String endpoint, {
 
-    /// REST API namespace
-    String namespace = wpNamespace,
+      /// Provide an API endpoint
+      String endpoint, {
 
-    /// Additional wordpress arguments
-    Map<String, dynamic>? args,
-  }) async {
+        /// REST API namespace
+        String namespace = wpNamespace,
+
+        /// Additional wordpress arguments
+        Map<String, dynamic>? args,
+      }) async {
     final uri = await _discover(site);
 
     //***************************** */
@@ -82,46 +92,47 @@ class WordPressAPI {
     //******************************************* */
     // FETCH REQUESTED DATA AND RETURN WP A RESPONSE
     //******************************************* */
-    try {
-      int? total, totalPages;
-      final res = await _dio.get(
-        '/$namespace/$endpoint',
-        queryParameters: args,
-      );
+    int? total, totalPages;
+    final res = await _dio.get(
+      '/$namespace/$endpoint',
+      queryParameters: args,
+    );
 
-      if (res.headers.value('x-wp-total') != null) {
-        total = int.tryParse(res.headers.value('x-wp-total')!);
-        totalPages = int.tryParse(res.headers.value('x-wp-totalpages')!);
-      }
-
-      return WPResponse(
-        data: res.data,
-        meta: WPMeta(
-          total: total,
-          totalPages: totalPages,
-        ),
-        statusCode: res.statusCode!,
-      );
-    } on DioError catch (e) {
-      Utils.logger.e(e.message);
-      rethrow;
-    } catch (e) {
-      rethrow;
+    if (res.headers.value('x-wp-total') != null) {
+      total = int.tryParse(res.headers.value('x-wp-total')!);
+      totalPages = int.tryParse(res.headers.value('x-wp-totalpages')!);
     }
+    return WPResponse(
+      data: res.data,
+      meta: WPMeta(
+        total: total,
+        totalPages: totalPages,
+      ),
+      statusCode: res.statusCode!,
+    );
   }
 
 // WP Standard Endpoints
 
   _ApplicationPasswords get application_passwords =>
       _ApplicationPasswords(this);
+
   _Posts get posts => _Posts(this);
+
   _Pages get pages => _Pages(this);
+
   _Media get media => _Media(this);
+
   _Categories get categories => _Categories(this);
+
   _Comments get comments => _Comments(this);
+
   _Users get users => _Users(this);
+
   _Search get search => _Search(this);
+
   _Taxonomies get taxonomies => _Taxonomies(this);
+
   _Tags get tags => _Tags(this);
 
 // WP Plugins Endpoints
@@ -152,12 +163,15 @@ Future<String> _discover(String site) async {
     // : Change logger to comment. Used only to debug
     // Utils.logger.i("HEADER: ${res.headers}");
     if (res.headers['link'] != null) {
-      final link = res.headers['link']!.first.split(';').first;
+      final link = res.headers['link']!
+          .first
+          .split(';')
+          .first;
       return link.substring(1, link.length - 1);
     }
     return "$_site/wp-json";
   } catch (e) {
-    Utils.logger.e(e);
+    WPUtils.logger.e(e);
     rethrow;
   }
 }
